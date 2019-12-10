c5c913e62674b82fe50972dde5a0461f1e388e4d
sean-hackett <seanhackett55@gmail.com>
2019-12-10 15:38:14 -0500

    Print loss, accuracy

--- anongan/trainer.py
+++ anongan/trainer.py
@@ -23,20 +23,25 @@ class Trainer(object):
 
     def train_discriminator(self, num_batches):
         for i in range(num_batches):
+            print(i)
             batch = next(self.batches)
             self.train_discriminator_batch(batch)
             self.batch_idx += 1
 
     def train_generator(self, num_batches):
         for i in range(num_batches):
+            print(i)
             batch = next(self.batches)
             self.train_generator_batch(batch)
             self.batch_idx += 1
 
     def train_both(self, num_batches):
         for i in range(num_batches):
+            print(i)
             batch = next(self.batches)
+            print('Discriminator:')
             self.train_discriminator_batch(batch)
+            print('Generator:')
             self.train_generator_batch(batch)
             self.batch_idx += 1
 
@@ -47,6 +52,9 @@ class Trainer(object):
 
         loss_real, accuracy_real = self.discriminator.train_on_batch([images], self.real)
         loss_fake, accuracy_fake = self.discriminator.train_on_batch([generated_images], self.fake)
+
+        print('Real loss: {}, Real accuracy: {}'.format(loss_real, accuracy_real))
+        print('Fake loss: {}, Fake accuracy: {}'.format(loss_fake, accuracy_fake))
 
         self.tensorboard.on_batch_end(self.batch_idx,   {
                                          'loss_real': loss_real,
@@ -61,6 +69,8 @@ class Trainer(object):
 
         loss, accuracy = self.full.train_on_batch(
                                     [images, masks], self.real)
+
+        print('Loss: {}, Accuracy: {}'.format(loss, accuracy))
 
         self.tensorboard.on_batch_end(self.batch_idx, {
                                          'loss_adv': loss,
